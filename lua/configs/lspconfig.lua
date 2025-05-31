5465507b27f9a43ef67354c1622a5789d3dbaadc
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-05-31 14:55:46 -0400

    lspconfig: lua_ls with love2d and vim

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "pyright", "glsl_analyzer", "lua_ls"}
+local servers = { "html", "cssls", "ts_ls", "pyright", "glsl_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,6 +16,27 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig["lua_ls"].setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    Lua = {
+      checkThirdParty = false,
+      telemetry = { enable = false },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          "C:/Users/tinnguyen/Documents/notes/sketches/love2d/3rd/love2d/library",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
 -- clangd
 lspconfig["clangd"].setup {
   filetypes = { "java", "c" }, -- Only enable for Java files
