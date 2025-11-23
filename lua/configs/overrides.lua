0d146b4ba6cf662a5ff881654d96f8a2323c723d
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-11-23 12:47:31 -0500

    config(treesitter): add proper config for textobject module

--- lua/configs/overrides.lua
+++ lua/configs/overrides.lua
@@ -137,17 +137,37 @@ M.treesitter = {
   },
   textobjects = {
     select = {
-      enable = true,
       lookahead = true,
-      keymaps = {
-        -- ["im"] = "@latex_block.inner",
-        ["am"] = { query = "@latex.outer", desc = "Non-Custom Latex Textobject" },
-        ["im"] = { query = "@latex.inner", desc = "Custom Textobject" },
-        -- ["im"] = { query = "@cookiecat.inner", desc = "Non-Custom Textobject" },
+      enable = true,
+    },
+    move = {
+      enable = true ,
+      goto_next_start = {
+        ["))"] = "@latex.outer"
       },
+      goto_previous_start = {
+        ["(("] = "@latex.outer"
+      }
+    }
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
     },
   },
-
 }
 
 M.telescope = {
