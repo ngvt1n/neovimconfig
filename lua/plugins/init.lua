54abd0f9cbecca4df17a5530fce50512a9cfec8b
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-11-23 12:58:49 -0500

    remove: vimtex

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,11 +12,12 @@ return {
   },
   {
     "lervag/vimtex",
-    lazy = false,     -- we don't want to lazy load VimTeX
+    enabled = false,
+    lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       vim.g.vimtex_view_general_viewer = 'okular'
-      vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+      vim.g.vimtex_view_general_options = 'file:@pdf#src:@line@tex'
       vim.g.vimtex_delim_list = {
         delim_math = {
           name = {
