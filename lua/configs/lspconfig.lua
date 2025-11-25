a59b11a888ca62afcc7f66b0c554640df2ce25fe
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-11-24 20:26:01 -0500

    chore: comment out emmylua_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,3 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "ts_ls", "glsl_analyzer" }
@@ -47,6 +46,41 @@ vim.lsp.config("lua_ls", {
     },
   },
 })
+vim.lsp.config("emmylua_ls", {
+  settings = {
+    -- ["$schema"] = "https://raw.githubusercontent.com/EmmyLuaLs/emmylua-analyzer-rust/refs/heads/main/crates/emmylua_code_analysis/resources/schema.json",
+    -- signature = {
+    --     detailSignatureHelper = true
+    -- },
+    -- hover = { enable = true },
+    -- diagnostics = {
+    --   enable = true,
+    --   enables = {"syntax-error", "param-type-not-match"},
+    -- },
+    -- runtime = {
+    --   requirePattern = {
+    --     '?.lua',
+    --     '?/init.lua',
+    --     -- 'lua/?.lua',
+    --     -- 'lua/?/init.lua',
+    --     -- '?/lua/?.lua',
+    --     -- '?/lua/?/init.lua',
+    --   },
+    --   extensions = {".lua", ".lua.txt"}
+    -- },
+    -- workspace = {
+    --   library = {
+    --     -- vim.fn.stdpath "data" .. "/lazy/NvChad/lua/nvchad",
+    --     vim.env.VIMRUNTIME,
+    --     vim.fn.expand '~/luarocks/share/lua/5.4',
+    --     vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+    --     'C:/Users/tinnguyen/AppData/Local/nvim-data/mason/packages/lua-language-server/meta/3rd/busted/library/',
+    --     'C:/Users/tinnguyen/AppData/Local/nvim-data/mason/packages/lua-language-server/meta/3rd/luassert/library/'
+    --   },
+    -- },
+  },
+})
+
 vim.lsp.enable "lua_ls"
 
 -- clangd
