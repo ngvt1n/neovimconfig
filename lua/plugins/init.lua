f7cef3f6c4f5dc7f0c122401208da115c10197ea
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-11-25 13:19:21 -0500

    lazy-lock: ui: v3.0 -> tin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 local overrides = require("configs.overrides")
 return {
   -- { "michaeljsmith/vim-indent-object", lazy = false },
+  { "nvchad/ui", url="https://github.com/ngvt1n/ui", branch="tin" },
   {
     {
       "pwntester/octo.nvim",
